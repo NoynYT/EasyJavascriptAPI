74ddb08131df7dc551183f9b4a89a423fb4e5ca6
NoynYT <dothiut0101@gmail.com>
2021-10-31 12:09:49 +0800

    Test v1

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const src=require("./src")
-const commands = {}
+// const src=require("./src")
+// const commands = {}
 
-for (const command of src){
-    commands[command] = require(`./src/${command}.js`)
+// for (const command of src){
+//     commands[command] = require(`./src/${command}.js`)
+// }
+
+// module.exports=commands
+
+module.exports = (text, name) => {
+    if (text == (undefined||null)) throw Error("Don't randomly call this API!")
+    if (text.length == 0) throw Error("Please put something in!")
+    
+    if (name) return console.log(`${name}: ${text}`)
+    else return console.log(`${text}`)
 }
-
-module.exports=commands
