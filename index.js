73ea16eecab5d2a90953f2d7cf076913cd6f8288
NoynYT <dothiut0101@gmail.com>
2021-10-31 11:18:05 +0800

    Test v1.1
    another one

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function printOut(text){
-    console.log(`System: ${text}`)
+module.exports = (text, name) => {
+    if (text == (undefined||null)) throw Error("Don't randomly call this API!")
+    if (text.length == 0) throw Error("Please put something in!")
+    
+    if (name) return console.log(`${name}: ${text}`)
+    else return console.log(`${text}`)
 }
